3a7e76a926ea280857fa56fdad666bcf084f8a18
EULA-Violator <lfork0@eq.edu.au>
2026-06-15 05:29:25 +1000

    working powershell

--- help/install/Install_Windows.ps1
+++ help/install/Install_Windows.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = 'Stop'
 #Ensure stopping if errors occur. This is important for installation scripts to prevent partial installations.
-mkdir "$HOME\fll"
+mkdir "$HOME\fll" -Force
 #makes fll directory
 Write-Host "Intstalling Git and Python via winget..."
 #Inform user of installing git and python
@@ -20,13 +20,13 @@ $Token = Read-Host "Enter your token (Get it from your GitHub settings): "
 #Get token
 git clone "https://$($Username):$($Token)@github.com/mrobinson-12/fll-2026"
 #Clone repo
-Write-Host "Repository cloned succesfully" -ForgroundColor Green
+Write-Host "Repository cloned succesfully" -ForegroundColor Green
 #Report succesful cloning
 cd fll-2026
 #Navigate to cloned repo
 Write-Host "Setting up python venv and installing required dependencies..."
 #inform user of next steps - python venv and dependencies
-python -m venv.venv
+python -m venv .venv
 #setup python virtual envirmnt
 .venv\Scripts\Activate.ps1
 #activate python virtual envirmnt
@@ -36,8 +36,8 @@ python -m pip install pybricks
 #install pybricks
 python -m pip install pybricksdev
 #install pybricksdev
-$User_name = Read-Host "Enter your name: "
-#get username
+#$User_name = Read-Host "Enter your name: "
+#get user name imma not use bc why tf is this in the bash
 $Useremail = Read-Host "Enter your GitHub email: "
 #get user email
 git config --global user.name "$Username"
